db8780109b1fccfbe48834e1a8100c12f284f2f7
Ken Urata <senseiurata@gmail.com>
2023-10-06 00:30:28 -0700

    Remove next image optimization so that images render
    .. on Github pages

--- docs/_next/static/chunks/app/page-3de6d5e4388e9284.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,327,178,444],{180:function(e,n,s){Promise.resolve().then(s.t.bind(s,3222,23)),Promise.resolve().then(s.t.bind(s,6685,23))}},function(e){e.O(0,[685,222,971,596,744],function(){return e(e.s=180)}),_N_E=e.O()}]);
