3cc98dca25f32e794a9bdd1fe37a703a7d1c95c4
Ken Urata <senseiurata@gmail.com>
2023-10-06 19:18:56 -0700

    Improve profile page layout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,19 +13,22 @@ export default function Profile () {
           <p className="my-2">YC Founder</p>
         </div>
 
-        <InnerContentTemplate
-          title="Profile"
-          nextActionText="See My Portfolio"
-          nextUrl="/portfolio"
-        >
-          <div className="flex-auto flex flex-col gap-4">
-            <p>
-              I’m Ken, a coder who’s worked with JavaScript, PHP, and React; I helped make vrv.co, taught React to folks, and even dabbled in English-Japanese software translations.</p>
-            <p>
-            <a href="/resume.pdf">Ken’s Resume (PDF)</a>
-            </p>
-          </div>
-        </InnerContentTemplate>
+        <div className="flex mt-6 md:mt-0">
+          <InnerContentTemplate
+            title="Profile"
+            nextActionText="See My Portfolio"
+            nextUrl="/portfolio"
+          >
+            <div className="flex-auto max-w-sm">
+              <p className="my-4">
+                I’m Ken, a coder who’s worked with JavaScript, PHP, and React; I helped make vrv.co, taught React to folks, and even dabbled in English-Japanese software translations.
+              </p>
+              <p className="my-4">
+                <a href="/resume.pdf">Ken’s Resume (PDF)</a>
+              </p>
+            </div>
+          </InnerContentTemplate>
+        </div>
       </>
     </OuterContentTemplate>
   );
